44bab905e90f7fef43dbf42660e514c1d76bb55b
agilemac-19 <nilesh.patel@agileinfoways.com>
2018-03-20 18:02:13 +0530

    data update

--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    var manageObjectContext : NSManagedObjectContext!
+    let imageView : UIImageView! = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView.image = UIImage(named: "1.jpeg")
+        
+        manageObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        self.createRepresetItem()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    func createRepresetItem() {
+        let personItem = Person(context: manageObjectContext)
+        
+        personItem.image = UIImagePNGRepresentation(imageView.image!)
+        personItem.name = "Nilesh"
+        personItem.mobile = "9636373449"
+        
+        do {
+            try self.manageObjectContext.save()
+        }catch {
+            print(error.localizedDescription)
+        }
+    }
+    
 
+    @IBAction func buttonAction(_ sender : AnyObject) {
+        let fetchRequest : NSFetchRequest<Person> = Person.fetchRequest()
+        
+        do{
+            let presrnt = try self.manageObjectContext.fetch(fetchRequest)
+            print(presrnt.first?.name)
+            print(presrnt.first?.mobile)
+        }catch {
+            print(error.localizedDescription)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
